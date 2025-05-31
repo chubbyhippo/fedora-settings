e047249141e3e59b3f05a6320c172268dcb0323e
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-05-31 14:26:33 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
-sudo dnf -y install dnf-plugins-core
 
+sudo dnf install -y dnf-plugins-core
+sudo dnf config-manager addrepo --from-repofile=https://mise.jdx.dev/rpm/mise.repo
+sudo dnf install -y mise
 echo 'eval "$(mise activate bash)"' >> ~/.bashrc
 
 
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl enable --now docker
