9ef1b137324ea6d0a619d0dc87c4e988a5cea9db
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-05-31 14:25:35 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 sudo dnf -y install dnf-plugins-core
 
+echo 'eval "$(mise activate bash)"' >> ~/.bashrc
+
 
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl enable --now docker
